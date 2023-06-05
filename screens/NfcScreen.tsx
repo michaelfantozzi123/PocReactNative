d3153eda77ab65a8f77075469fb948a5f2d33ea7
StefanMarais <stefan.marais@kerridgecs.com>
2023-06-05 16:16:10 +0200

    remove broken nfc

--- screens/NfcScreen.tsx
+++ screens/NfcScreen.tsx
@@ -4,40 +4,40 @@ import NfcManager, { NfcTech } from "react-native-nfc-manager";
 
 type Props = {};
 
-NfcManager.start();
+// NfcManager.start();
 
 const NfcScreen = (props: Props) => {
-  async function readNdef() {
-    try {
-       console.warn("Tag");
-      // register for the NFC tag with NDEF in it
-      await NfcManager.requestTechnology(NfcTech.Ndef);
-      // the resolved tag object will contain `ndefMessage` property
-      const tag = await NfcManager.getTag();
-      console.warn("Tag found", tag);
-    } catch (ex) {
-      console.warn("Oops!", ex);
-    } finally {
-      // stop the nfc scanning
-      NfcManager.cancelTechnologyRequest();
-    }
-  }
+  // async function readNdef() {
+  //   try {
+  //      console.warn("Tag");
+  //     // register for the NFC tag with NDEF in it
+  //     await NfcManager.requestTechnology(NfcTech.Ndef);
+  //     // the resolved tag object will contain `ndefMessage` property
+  //     const tag = await NfcManager.getTag();
+  //     console.warn("Tag found", tag);
+  //   } catch (ex) {
+  //     console.warn("Oops!", ex);
+  //   } finally {
+  //     // stop the nfc scanning
+  //     NfcManager.cancelTechnologyRequest();
+  //   }
+  // }
 
-  return (
-    <View style={styles.wrapper}>
-      <TouchableOpacity onPress={readNdef}>
-        <Text>Scan a Tag</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  // return (
+  //   <View style={styles.wrapper}>
+  //     <TouchableOpacity onPress={readNdef}>
+  //       <Text>Scan a Tag</Text>
+  //     </TouchableOpacity>
+  //   </View>
+  // );
 };
 
 export default NfcScreen;
 
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
