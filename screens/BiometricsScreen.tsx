f9fa2338fb1a31ae5455809939396bb5603a58af
StefanMarais <stefan.marais@kerridgecs.com>
2023-06-05 13:19:21 +0200

    biometrics

--- screens/BiometricsScreen.tsx
+++ screens/BiometricsScreen.tsx
@@ -1,16 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from "react";
+import { Alert, View, StyleSheet } from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
 
-type Props = {}
+type Props = {};
 
 const BiometricsScreen = (props: Props) => {
+  let authSuccess = false;
+  useEffect(() => {
+    (async () => {
+      const hasHardware = await LocalAuthentication.hasHardwareAsync();
+      if (!hasHardware) {
+        Alert.alert("Device doesn't support biometric authentication.");
+        return;
+      }
+
+      const biometricRecords = await LocalAuthentication.isEnrolledAsync();
+      if (!biometricRecords) {
+        Alert.alert("No identities are enrolled.");
+        return;
+      }
+
+      const authOptions = {
+        promptMessage: "Authenticate to continue",
+        cancelLabel: "Cancel",
+        fallbackLabel: "Use passcode",
+      };
+
+      const result = await LocalAuthentication.authenticateAsync(authOptions);
+
+      if (result.success) {
+        Alert.alert("Authentication succeeded!");
+      } else {
+        Alert.alert("Authentication failed!");
+      }
+    })();
+  }, []);
+
   return (
     <View>
-      <Text>BiometricsScreen</Text>
+      
     </View>
-  )
-}
+  );
+};
 
-export default BiometricsScreen
+export default BiometricsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
